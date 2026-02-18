9781b5b45925fc2253d323390e540ad9972b1465
STEPS0N <stepan.klimoff@mail.ru>
2026-02-18 22:02:05 +0500

    Разработанный модуль пользователей. Климов

---
+++ Pages/Users/Add.xaml.cs
@@ -0,0 +1,83 @@
+﻿using KlimovPR29.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KlimovPR29.Pages.Users
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Page
+    {
+        public ClubsContext AllClub = new ClubsContext();
+
+        public Models.Users Users;
+
+        public Main Main;
+
+        public Add(Main Main, Models.Users Users = null)
+        {
+            InitializeComponent();
+            this.Users = Users;
+            this.Main = Main;
+
+            foreach (Models.Clubs Club in AllClub.Clubs)
+            {
+                club.Items.Add(Club.Name);
+            }
+            club.Items.Add("Выберите...");
+
+            if (Users != null)
+            {
+                this.FIO.Text = Users.FIO;
+                this.RentDate.Text = Users.RentStart.ToString("yyyy-MM-dd");
+                this.RentTime.Text = Users.RentStart.ToString("HH:mm");
+                this.Duration.Text = Users.Duration.ToString();
+                club.SelectedItem = AllClub.Clubs.Where(x => x.Id == Users.IdClub).First().Name;
+                btn.Content = "Изменить:";
+            }
+        }
+
+        private void AddUser(object sender, RoutedEventArgs e)
+        {
+            DateTime DTRentStart = new DateTime();
+            DateTime.TryParse(this.RentDate.Text, out DTRentStart);
+            DTRentStart = DTRentStart.Add(TimeSpan.Parse(this.RentTime.Text));
+
+            if (Users == null)
+            {
+                Users = new Models.Users();
+
+                Users.FIO = this.FIO.Text;
+                Users.RentStart = DTRentStart;
+                Users.Duration = Convert.ToInt32(this.Duration.Text);
+                Users.IdClub = AllClub.Clubs.Where(x => x.Name == club.SelectedItem).First().Id;
+
+                this.Main.AllUsers.Users.Add(this.Users);
+            }
+            else
+            {
+                Users.FIO = this.FIO.Text;
+                Users.RentStart = DTRentStart;
+                Users.Duration = Convert.ToInt32(this.Duration.Text);
+                Users.IdClub = AllClub.Clubs.Where(x => x.Name == club.SelectedItem).First().Id;
+            }
+
+            this.Main.AllUsers.SaveChanges();
+
+            MainWindow.init.OpenPage(new Pages.Users.Main());
+        }
+    }
+}
---
+++ Pages/Users/Elements/Item.xaml.cs
@@ -0,0 +1,55 @@
+﻿using KlimovPR29.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KlimovPR29.Pages.Users.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Item.xaml
+    /// </summary>
+    public partial class Item : UserControl
+    {
+        public ClubsContext AllClub = new ClubsContext();
+
+        Main Main;
+
+        Models.Users Users;
+
+        public Item(Main Main, Models.Users Users)
+        {
+            InitializeComponent();
+            this.Main = Main;
+            this.Users = Users;
+
+            this.FIO.Text = Users.FIO;
+            this.RentDate.Text = Users.RentStart.ToString("yyyy-MM-dd");
+            this.RentTime.Text = Users.RentStart.ToString("HH:mm");
+            this.Duration.Text = Users.Duration.ToString();
+            this.Club.Text = AllClub.Clubs.Where(x => x.Id == Users.IdClub).First().Name;
+        }
+
+        private void EditUser(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPage(new Pages.Users.Add(this.Main, this.Users));
+        }
+
+        private void DeleteUser(object sender, RoutedEventArgs e)
+        {
+            Main.AllUsers.Remove(Users);
+            Main.AllUsers.SaveChanges();
+            Main.parent.Children.Remove(this);
+        }
+    }
+}
